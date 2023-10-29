d1c57a6874768050a8f4b0792f55c9c99f1fc6fa
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2023-10-29 14:43:31 -0500

    Added 5 Day Forecast
    Added the five day forecast function of the app.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// List variables
+// List variables so I can concatenate the fetch API
 const APIKey = '841ae18f93498c5371706f226872863a';
 var city;
-const openWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=841ae18f93498c5371706f226872863a";
+const openWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=841ae18f93498c5371706f226872863a';
 
+// Added eventListener so user can type in their city and click a search button
 document.querySelector('.btn').addEventListener('click', function() {
     const city = document.getElementById('cityInput').value;
-
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial')
         .then(function(response) {
             return response.json();
@@ -17,21 +17,37 @@ document.querySelector('.btn').addEventListener('click', function() {
             const cityName = data.name;
             const currentDate = new Date().toLocaleDateString(); 
 
-
+// Adjusts the text in html to display the weather API's results
             document.getElementById('cityName').innerText = cityName;
             document.getElementById('currentDate').innerText = currentDate;
-            document.getElementById('temperature').innerText = temperature;
-            document.getElementById('humidity').innerText = humidity;
-            document.getElementById('windSpeed').innerText = windSpeed;
+            document.getElementById('temperature').innerText = temperature + 'degrees';
+            document.getElementById('humidity').innerText = humidity + '%';
+            document.getElementById('windSpeed').innerText = windSpeed + 'MPH';
             console.log(data);
         })
-})
 
-// When user searches for the city, display current conditions and 5-day forecast. Both need to include city name, date, icon of weather conditions, temp, humidity, and wind speed
-
-
-
-// Create a set of clickable search history buttons below the search bar that list previous searches using local storage.
-
-
-
+// Created a separate fetch function within the eventListener to reach the forecast. I chose 12:00 as an ideal forecast time since it's the middle of the day and most likely the average time a user would expect.
+    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey + '&units=imperial')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    const dailyForecast = data.list.filter(function(forecast) {
+        return forecast.dt_txt.includes('12:00:00');
+    });
+// Created the for loop to get each date's conditions. I'm still working on adjusting the date to display in MM/DD/YYYY format
+    for (let i = 0; i < dailyForecast.length; i++) {
+        const date = dailyForecast[i].dt_txt.split(' ')[0];
+        const temperature = dailyForecast[i].main.temp;
+        const humidity = dailyForecast[i].main.humidity;
+        const windSpeed = dailyForecast[i].wind.speed;
+        
+    document.getElementById(`day${i + 1}`).innerHTML =
+    date +
+    'Temperature: ' + temperature + 'degrees' +
+    'humidity: ' + humidity + '%' +
+    'windSpeed: ' + windSpeed + 'MPH' 
+    }
+  });
+});
+// TODO: Use Local Storage to create a Search History set of buttons that will then display those cities again
