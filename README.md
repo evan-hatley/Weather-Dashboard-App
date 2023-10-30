aae0debef834edad5a0e60193cdeccb3162a70bf
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2023-10-29 19:06:23 -0500

    Updated JS, CSS, README
    Simplified the JS and rewrote some code to make the HTML more dynamic and readable. Also applied some basic stylings so the page looks nicer. Updated the README to provide documentation

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
 # Weather-Dashboard-App
-This is an application using the 5 Day Weather Forecast API to fetch the latest weather data.
 
-84be11ea153cacbd59c2f6fe28b814e9
+## Description
+This is an application using the OpenWeatherMap API to fetch the latest weather data conditions as well as a 5 day forecast based on a user's input. It's a simple weather service designed to learn how to fetch server side APIs through the use of API keys. The Current Weather section includes:
+- Date  
+- City  
+- Temperature  
+- Humidity  
+- Wind Speed  
+
+The Five Day Forecast lists the same properties.
+
+## User Story
+AS A traveler  
+I WANT to see the weather outlook for multiple cities  
+SO THAT I can plan a trip accordingly  
+
+## Acceptance Criteria
+GIVEN a weather dashboard with form inputs  
+WHEN I search for a city  
+THEN I am presented with current and future conditions for that city and that city  is added to the search history  
+WHEN I view current weather conditions for that city  
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed  
+WHEN I view future weather conditions for that city  
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity  
+WHEN I click on a city in the search history  
+THEN I am again presented with current and future conditions for that city  
+
+## How to Use
+This is a simple weather app where the user just needs to input a city, and the API will use the geographic coordinates to display the current and future weather.
+
+## Limitations
+As of now, the application is lacking the ability to save user's search information and display previous search buttons. Most of the Local Storage code is written, but I'm working to debug the createElement button.
+
+## Application Link
+The deployed application can be found at 
--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 const APIKey = '841ae18f93498c5371706f226872863a';
 var city;
 const openWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=841ae18f93498c5371706f226872863a';
+let searchHistory = []
+
 
 // Added eventListener so user can type in their city and click a search button
 document.querySelector('.btn').addEventListener('click', function() {
     const city = document.getElementById('cityInput').value;
+
+    searchHistory.push(city);
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+// sets the fetch for the Current Weather. Iused two separate fetch requests to get different results for this and the five day forecast
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey + '&units=imperial')
         .then(function(response) {
             return response.json();
@@ -16,16 +22,14 @@ document.querySelector('.btn').addEventListener('click', function() {
             const windSpeed = data.wind.speed;
             const cityName = data.name;
             const currentDate = new Date().toLocaleDateString(); 
-
 // Adjusts the text in html to display the weather API's results
             document.getElementById('cityName').innerText = cityName;
             document.getElementById('currentDate').innerText = currentDate;
-            document.getElementById('temperature').innerText = temperature + 'degrees';
+            document.getElementById('temperature').innerText = temperature + ' degrees';
             document.getElementById('humidity').innerText = humidity + '%';
             document.getElementById('windSpeed').innerText = windSpeed + 'MPH';
             console.log(data);
         })
-
 // Created a separate fetch function within the eventListener to reach the forecast. I chose 12:00 as an ideal forecast time since it's the middle of the day and most likely the average time a user would expect.
     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIKey + '&units=imperial')
   .then(function(response) {
@@ -36,18 +40,38 @@ document.querySelector('.btn').addEventListener('click', function() {
         return forecast.dt_txt.includes('12:00:00');
     });
 // Created the for loop to get each date's conditions. I'm still working on adjusting the date to display in MM/DD/YYYY format
-    for (let i = 0; i < dailyForecast.length; i++) {
-        const date = dailyForecast[i].dt_txt.split(' ')[0];
-        const temperature = dailyForecast[i].main.temp;
-        const humidity = dailyForecast[i].main.humidity;
-        const windSpeed = dailyForecast[i].wind.speed;
-        
-    document.getElementById(`day${i + 1}`).innerHTML =
-    date +
-    'Temperature: ' + temperature + 'degrees' +
-    'humidity: ' + humidity + '%' +
-    'windSpeed: ' + windSpeed + 'MPH' 
-    }
+for (let i = 0; i < dailyForecast.length; i++) {
+  // Created new HTML elements for each piece of data, then dynamically adding the API info to each div. Then finally using append.child to update the text fields i nthe divs.
+  const dateElement = document.createElement('div');
+  const tempElement = document.createElement('div');
+  const humidityElement = document.createElement('div');
+  const windElement = document.createElement('div');
+
+  dateElement.innerText = dailyForecast[i].dt_txt.split(' ')[0];
+  tempElement.innerText = 'Temperature: ' + dailyForecast[i].main.temp + ' degrees';
+  humidityElement.innerText = 'Humidity: ' + dailyForecast[i].main.humidity + '%';
+  windElement.innerText = 'Wind Speed: ' + dailyForecast[i].wind.speed + ' MPH';
+
+  const forecastDayDiv = document.getElementById(`day${i + 1}`);
+  forecastDayDiv.appendChild(dateElement);
+  forecastDayDiv.appendChild(tempElement);
+  forecastDayDiv.appendChild(humidityElement);
+  forecastDayDiv.appendChild(windElement);
+}
   });
 });
-// TODO: Use Local Storage to create a Search History set of buttons that will then display those cities again
+// IN PROGRESS - Debug the querySelector so that the button will appear.
+document.querySelector('.btn').addEventListener('click', function() {
+    const city = document.getElementById('cityInput').value;
+    // This next code snippet was to ensure that each city would be displayed in the search history once
+    if (!searchHistory.includes(city)) {
+      searchHistory.push(city);
+      localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+      // This is supposed to create a new button elemnent to the HTML but even after hours of debugging, I can't get it to render
+      const btn = document.createElement('button');
+      btn.textContent = city;
+      btn.addEventListener('click', function() {
+      });
+      document.getElementById('searchHistoryContainer').appendChild(btn);
+    }
+  });
